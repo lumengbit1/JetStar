068c5c564bfdcf18273c5407826b4461d2c1054c
Administrator <lumengbit1@gmail.com>
2022-02-26 16:26:07 +1100

    add AppNotFound test case

---
+++ src/test/notfound.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'jest-styled-components';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import AppNotFound from '../components/notfound';
+
+describe('AppNotFound Test Cases', () => {
+  it('1: Component shoule be rendered correctly', async () => {
+    const { container } = render(
+      <AppNotFound />
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
