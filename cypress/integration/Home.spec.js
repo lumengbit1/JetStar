36b6c188be69cd0953d202f5ea18cd14a1f36cfd
Administrator <lumengbit1@gmail.com>
2022-02-26 17:52:51 +1100

    add cypress test case with correct command

--- cypress/integration/Home.spec.js
+++ cypress/integration/Home.spec.js
@@ -1,7 +1,9 @@
 describe('The Home Page', () => {
-  it('1. put correct PLACE command should display in the command list', () => {
+  beforeEach(() => {
     cy.visit('/');
+  })
 
+  it('1. put correct PLACE command should display in the command list', () => {
     cy.get('input')
     .type('PLACE,0,0,NORTH')
     .get(`[aria-label="run-button"]`)
@@ -9,6 +11,66 @@ describe('The Home Page', () => {
     .get(`[aria-label="command-list"]`)
     .should(($ele)=>{
       expect($ele).to.contain('PLACE (0, 0, \'NORTH\')');
+    });
+  })
+
+  it('2. put correct MOVE command should display in the command list', () => {
+    cy.get('input')
+    .type('PLACE,0,0,NORTH')
+    .get(`[aria-label="run-button"]`)
+    .click()
+    .get('input')
+    .type('MOVE')
+    .get(`[aria-label="run-button"]`)
+    .click()
+    .get(`[aria-label="command-list"]`)
+    .should(($ele)=>{
+      expect($ele).to.contain('MOVE');
+    });
+  })
+
+  it('3. put correct LEFT command should display in the command list', () => {
+    cy.get('input')
+    .type('PLACE,0,0,NORTH')
+    .get(`[aria-label="run-button"]`)
+    .click()
+    .get('input')
+    .type('LEFT')
+    .get(`[aria-label="run-button"]`)
+    .click()
+    .get(`[aria-label="command-list"]`)
+    .should(($ele)=>{
+      expect($ele).to.contain('LEFT');
+    });
+  })
+
+  it('4. put correct RIGHT command should display in the command list', () => {
+    cy.get('input')
+    .type('PLACE,0,0,NORTH')
+    .get(`[aria-label="run-button"]`)
+    .click()
+    .get('input')
+    .type('RIGHT')
+    .get(`[aria-label="run-button"]`)
+    .click()
+    .get(`[aria-label="command-list"]`)
+    .should(($ele)=>{
+      expect($ele).to.contain('RIGHT');
+    });
+  })
+
+  it('5. put correct REPORT command should display in the command list', () => {
+    cy.get('input')
+    .type('PLACE,0,0,NORTH')
+    .get(`[aria-label="run-button"]`)
+    .click()
+    .get('input')
+    .type('REPORT')
+    .get(`[aria-label="run-button"]`)
+    .click()
+    .get(`[aria-label="command-list"]`)
+    .should(($ele)=>{
+      expect($ele).to.contain('REPORT( ) => OUTPUT: 0, 0, NORTH');
     });
   })
 })
