49ca21079c5667436f3c3f14b3da26eb46135b87
Administrator <lumengbit1@gmail.com>
2022-03-03 16:34:21 +1100

    remove useless params

--- src/router/routers.jsx
+++ src/router/routers.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Redirect, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from '../components/Home';
 import MainRouter from '../components/MainRouter';
 import AppNotFound from '../components/notfound';
